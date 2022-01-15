0ab7d04bdcd6e25c89712af551791f81bddc70b4
Joe <joebreilly@yahoo.ie>
2022-01-15 15:23:21 +0000

    Initial commit set up react

---
+++ todo/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+// Importing components
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Picture Perfect Tour List</h1>
+    </header>
+    <Form />
+    </div>
+  );
+}
+
+export default App;
